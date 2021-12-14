5c9438eb787d1912c374cc1505da4d116ec3af74
meidar.shlooshsimilarweb.com <meidar.shloosh@similarweb.com>
2021-12-14 18:37:52 +0200

    added option to evaluate on predicted data

--- modules/CDD.py
+++ modules/CDD.py
@@ -3,39 +3,37 @@ import json
 import numpy as np
 from utils.NpEncoder import NpEncoder
 
+
 class CDD():
-    def __init__(self, path=None, prev_sample:list=None):
+    def __init__(self, path=None, prev_sample: list = None, apply_hist=True, hist_bins=10):
         if path:
             with open(path) as f:
                 self.cache = json.load(f)
         elif prev_sample is not None:
             self.cache = {
-                "hist_bins":  len(prev_sample),
+                "hist_bins":  hist_bins,
+                "apply_hist": apply_hist,
                 "prev_samples": [list(prev_sample)]
             }
         else:
-            raise Exception("Must provide either previous sample histogram to compare or cache path")
-
-    @staticmethod
-    def get_hist(model, X, hist_bins=20):
-        y_pred = model.predict_proba(X)
-        hist = np.histogram(y_pred, hist_bins)
-        return hist
+            raise Exception("Must provide either previous sample to compare or cache path")
 
     def evaluate(self, model, X, save_to_cache=False):
         if not hasattr(model, 'predict_proba'):
             raise Exception("Model doesn't have soft prediction method")
         y_pred = model.predict_proba(X)
-        hist = np.histogram(y_pred, self.cache["hist_bins"])[0].tolist()
+        dist = y_pred
+        if self.cache["apply_hist"]:
+            dist = np.histogram(y_pred, self.cache["hist_bins"])[0].tolist()
         ks_stat_res = []
         ks_pval_res = []
         for prev_sample in self.cache["prev_samples"]:
-            res = stats.ks_2samp(hist, prev_sample)
+            res = stats.ks_2samp(dist, prev_sample)
             ks_stat_res.append(res[0])
             ks_pval_res.append(res[1])
 
         if save_to_cache:
-            self.cache["prev_samples"].append(hist)
+            self.cache["prev_samples"].append(dist)
         return ks_stat_res, ks_pval_res
 
     def save(self, path):
