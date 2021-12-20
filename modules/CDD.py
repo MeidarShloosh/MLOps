6c72e7ff7755a7f9d0fcb27989eecb2bdb81ef67
meidar.shlooshsimilarweb.com <meidar.shloosh@similarweb.com>
2021-12-20 16:38:32 +0200

    removed histogram from CDD and added add_sample method

--- modules/CDD.py
+++ modules/CDD.py
@@ -1,3 +1,5 @@
+from collections import Sequence
+
 from scipy import stats
 import json
 import numpy as np
@@ -5,26 +7,29 @@ from utils.NpEncoder import NpEncoder
 
 
 class CDD():
-    def __init__(self, path=None, prev_sample: list = None, apply_hist=True, hist_bins=50):
+    def __init__(self, path=None, prev_sample: list = None):
         """
         Loaded previous predict histograms if available or uses the given sample to compare to.
         :param path: path to cache file (json type)
         :param prev_sample: list of previous samples
-        :param apply_hist: flag to indicate if need to apply histogram on the observations
-        :param hist_bins: number of histogram bins
         """
         if path is not None:
             with open(path) as f:
                 self.cache = json.load(f)
         elif prev_sample is not None:
-            prev_sample = list(prev_sample) if not apply_hist else np.histogram(prev_sample, hist_bins, density=True)[0].tolist()
-            self.cache = {
-                "hist_bins":  hist_bins,
-                "apply_hist": apply_hist,
-                "prev_samples": [prev_sample]
-            }
+            self.add_sample(prev_sample)
         else:
-            raise Exception("Must provide either previous sample histogram to compare or cache path")
+            self.cache = None
+
+    def add_sample(self, sample: Sequence):
+        """
+        Adds another sample to compare in the evaluation phase
+        :param sample: list of previouse predictions
+        """
+        if self.cache is None:
+            self.cache = {"prev_samples": [sample]}
+        else:
+            self.cache["prev_samples"].append(sample)
 
     def evaluate(self, y, save_to_cache=False):
         """
@@ -34,18 +39,17 @@ class CDD():
         :param save_to_cache: A flag that indicates if to save the prediction histogram to cache
         :return: ks_statistics with previous histograms, pvalue of ks results with previous histograms
         """
-        dist = y
-        if self.cache["apply_hist"]:
-            dist = np.histogram(y, self.cache["hist_bins"], density=True)[0].tolist()
+        if self.cache is None:
+            raise Exception("No previous samples to evaluate")
         ks_stat_res = []
         ks_pval_res = []
         for prev_sample in self.cache["prev_samples"]:
-            res = stats.ks_2samp(dist, prev_sample)
+            res = stats.ks_2samp(y, prev_sample)
             ks_stat_res.append(res[0])
             ks_pval_res.append(res[1])
 
         if save_to_cache:
-            self.cache["prev_samples"].append(dist)
+            self.cache["prev_samples"].append(y)
         return ks_stat_res, ks_pval_res
 
     def save(self, path):
